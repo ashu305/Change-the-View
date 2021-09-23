50f080956790ff7c33b726c7816882cb683937ee
Ashutosh Sharma <iamashu2002@gmail.com>
2021-09-23 10:54:32 +0530

    Day 3 challange complete

---
+++ code.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll(".controls input")
+
+function handelUpdate(){
+
+    const suffix = this.dataset.sizing || '';
+
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => {
+   return input.addEventListener('change', handelUpdate)
+})
+
+inputs.forEach(input => input.addEventListener('mousemove', handelUpdate))
